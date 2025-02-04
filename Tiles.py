6afe5e9e93d0af286653e70a6e9607618c331a60
mustafa.faridee@gmail.com <mustafa.faridee@gmail.com>
2025-02-04 17:05:31 +1100

    Changed tiles from sprites to rects

--- Tiles.py
+++ Tiles.py
@@ -28,42 +28,38 @@ class Tile(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(topleft=(x, y))
         self.mask = pygame.mask.from_surface(self.image)
 
+BG_tile_rects = []
+solid_tile_rects = []
+
 def draw_tilemap(screen, ldtk_data):
-    solid_tile_sprites = pygame.sprite.Group()
-    BG_tile_sprites = pygame.sprite.Group()
+    global BG_tile_rects, solid_tile_rects
+    BG_tile_rects.clear()
+    solid_tile_rects.clear()
 
     for layer in ldtk_data['levels'][0]['layerInstances']:
         if layer['__type'] == 'IntGrid' and layer['__identifier'] == 'IntGrid_layer':
             for i, tile_id in enumerate(layer['intGridCsv']):
                 x = (i % layer['__cWid']) * TILE_SIZE
                 y = (i // layer['__cWid']) * TILE_SIZE
-
                 if tile_id == 1:  # Solid
                     tile_image = tiles[1]
-                    solid_tile = Tile(tile_image, x, y)
-                    solid_tile_sprites.add(solid_tile)
+                    solid_rect = pygame.Rect(x, y, TILE_SIZE, TILE_SIZE)
+                    solid_tile_rects.append(solid_rect)
                 elif tile_id == 0:  # BG
                     tile_image = tiles[0]
-                    BG_tile = Tile(tile_image, x, y)
-                    BG_tile_sprites.add(BG_tile)
+                    screen.blit(tile_image, (x, y))
+                    BG_rect = pygame.Rect(x, y, TILE_SIZE, TILE_SIZE)
+                    BG_tile_rects.append(BG_rect)
 
+    # Draw autolayer tiles
     for layer in ldtk_data['levels'][0]['layerInstances']:
         if 'autoLayerTiles' in layer:
             for tile in layer['autoLayerTiles']:
                 src_x = tile['src'][0]
                 src_y = tile['src'][1]
-                dest_x = tile['px'][0] * TILE_SIZE//8
-                dest_y = tile['px'][1] * TILE_SIZE//8
+                dest_x = tile['px'][0] * TILE_SIZE // 8
+                dest_y = tile['px'][1] * TILE_SIZE // 8
 
                 tile_image = get_tile(src_x // 8, src_y // 8)
                 tile_image = pygame.transform.scale(tile_image, (TILE_SIZE, TILE_SIZE))
                 screen.blit(tile_image, (dest_x, dest_y))
-
-                # Assuming all autolayer tiles are solid
-                solid_tile = Tile(tile_image, dest_x, dest_y)
-                solid_tile_sprites.add(solid_tile)
-
-    solid_tile_sprites.draw(screen)
-    BG_tile_sprites.draw(screen)
-    
-    return solid_tile_sprites
